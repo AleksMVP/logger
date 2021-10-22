05ade4eee23ef76728216428e9ffa9b53e530f49
ZiJer <zijer23@gmail.com>
2021-10-22 15:01:55 +0000

    added formatting & variadic methods

--- logger.go
+++ logger.go
@@ -6,7 +6,18 @@ import (
 )
 
 type ILogger interface {
+	Infof(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+	Warningf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Warning(args ...interface{})
+	Error(args ...interface{})
+
 	LogInfo(pkg string, method string, msg string)
+	LogWarn(pkg string, method string, msg string)
 	LogWarning(pkg string, method string, msg string)
 	LogError(pkg string, method string, err error)
 	LogAccess(r *http.Request, status int, worktime time.Duration) 
--- logger/logger.go
+++ logger/logger.go
@@ -3,6 +3,8 @@ package logger
 import (
 	"io"
 	"net/http"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -23,18 +25,95 @@ func NewLogger(writer io.Writer) *Logger {
 	return lgr
 }
 
+func getCallerInfo() (fn string, pkg string) {
+	pc, _, _, ok := runtime.Caller(1)
+	if !ok {
+		return "unknown", "unknown"
+	}
+
+	name := runtime.FuncForPC(pc).Name()
+	slash := strings.LastIndexByte(name, '/')
+	if slash < 0 {
+		slash = 0
+	}
+	dot := strings.LastIndexByte(name[slash:], '.') + slash
+
+	return name[dot+1:], name[:dot]
+}
+
+func (l *Logger) Infof(format string, args ...interface{}) {
+	fn, pkg := getCallerInfo()
+	l.WithFields(logrus.Fields{
+		"source":  pkg,
+		"function": fn,
+	}).Infof(format, args...)
+}
+
+func (l *Logger) Warnf(format string, args ...interface{}) {
+	fn, pkg := getCallerInfo()
+	l.WithFields(logrus.Fields{
+		"source":  pkg,
+		"function": fn,
+	}).Warnf(format, args...)
+}
+
+func (l *Logger) Warningf(format string, args ...interface{}) {
+	fn, pkg := getCallerInfo()
+	l.WithFields(logrus.Fields{
+		"source":  pkg,
+		"function": fn,
+	}).Warningf(format, args...)
+}
+
+func (l *Logger) Errorf(format string, args ...interface{}) {
+	fn, pkg := getCallerInfo()
+	l.WithFields(logrus.Fields{
+		"source":  pkg,
+		"function": fn,
+	}).Errorf(format, args...)
+}
+
+func (l *Logger) Info(args ...interface{}) {
+	fn, pkg := getCallerInfo()
+	l.WithFields(logrus.Fields{
+		"source":  pkg,
+		"function": fn,
+	}).Info(args...)
+}
+
+func (l *Logger) Warn(args ...interface{}) {
+	fn, pkg := getCallerInfo()
+	l.WithFields(logrus.Fields{
+		"source":  pkg,
+		"function": fn,
+	}).Warn(args...)
+}
+
+func (l *Logger) Error(args ...interface{}) {
+	fn, pkg := getCallerInfo()
+	l.WithFields(logrus.Fields{
+		"source":  pkg,
+		"function": fn,
+	}).Error(args...)
+}
+
 func (l *Logger) LogInfo(pkg string, method string, msg string) {
 	l.WithFields(logrus.Fields{
-		"package":  pkg,
+		"source":  pkg,
 		"function": method,
 	}).Info(msg)
 }
 
-func (l *Logger) LogWarning(pkg string, method string, msg string) {
+func (l *Logger) LogWarn(pkg string, method string, msg string) {
 	l.WithFields(logrus.Fields{
 		"package":  pkg,
 		"function": method,
 	}).Warn(msg)
+}
+
+// Deprecated: use LogWarn
+func (l *Logger) LogWarning(pkg string, method string, msg string) {
+	l.LogWarn(pkg, method, msg)
 }
 
 func (l *Logger) LogError(pkg string, method string, err error) {
